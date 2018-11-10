c19f541a4dad1af6b41f4e7909e456d07648c685
JasonChoi <ccc11203@gmail.com>
2018-11-10 20:39:11 +0900

    Chapter 3_7 Binary Numbers

---
+++ Chapter3/Chapter3_BinaryNumbers.cpp
@@ -0,0 +1,52 @@
+/*
+HongJeongMo C++
+
+Chapter 3_7
+Binary Numbers
+*/
+
+#include <iostream>
+
+int getChapter3_7()
+{
+	using namespace std;
+
+	/*
+	Decimal number
+	0
+	1
+	2
+	3
+	4
+	5
+	6
+	7
+	8
+	9
+	10 = 10^1 + 0
+	11 = 10^1 + 1
+	12 = 10^1 + 2
+
+
+	337 = 300 + 30 + 7 = 10^2 *3 + 10^1 *3 + 10^0 * 7
+	
+	
+	*/
+
+	/*
+	binary number 
+	0
+	1
+	10 = 2^1*1 + 2^0*0 = 2
+	11 = 2^1*1 + 2^0*1 = 3
+
+
+	 11
+	+ 1
+	-----
+	100
+	*/
+
+
+	return 0;
+}
