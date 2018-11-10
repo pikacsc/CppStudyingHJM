c1b042769630a5c7796c5a29f1d95cb164e85463
JasonChoi <ccc11203@gmail.com>
2018-11-10 20:45:11 +0900

    Chapter 5_1 Control Flow, exit(0);

---
+++ Chapter5_controlFlow/1_ControlFlow.cpp
@@ -0,0 +1,37 @@
+/*
+HongJeongMo C++
+
+Chapter 5_1 
+Control Flow
+
+Halt
+
+Jump
+goto, break, continue
+
+
+Conditional branches
+if, switch
+
+Loops
+while, do while, for
+
+Exceptions
+try, catch, throw
+*/
+
+#include <iostream>
+#include <cstdlib> // for using "exit(0);"
+int ControlFlow()
+{
+	using namespace std;
+	cout << "I love you " << endl;
+
+
+	exit(0);//you can exit the program immediately for emergency
+/*
+	cout << "I love you, too" << endl;
+*/
+
+	return 0;
+}
