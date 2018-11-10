cc93d1995beb98aa761ded6678c615b926c72c44
JasonChoi <ccc11203@gmail.com>
2018-11-10 20:28:03 +0900

    Chapter 1_6 keywords, identifiers

---
+++ Chapter1_6/main.cpp
@@ -0,0 +1,41 @@
+/*
+HongJeongMo C++
+
+Chapter 1_6 keywords, identifiers
+*/
+
+//1. you can't name variable with "reserved keywords" ex)void, int, return, etc
+
+//2. When you name variable, it shouldn't be start with number
+
+//3. no empty space in variable name, but you can name with underscore
+
+#include <iostream>
+using namespace std;
+
+
+void GetNumber() // Not like variable, mostly function names have been capitalized and pattern like V + N ( AddNumber, MixNumber, GetCount, etc)
+{
+
+}
+
+
+
+int main()
+{
+	// various styles
+	int num_apples; // num apples ¢º X
+	int nApples; 
+	int n_oranges;  
+	int _apples;
+
+	int total;
+	int some2; // 2some ¢º X
+	int TotalCustomers;
+	
+
+	GetNumber();
+	
+
+	return 0;
+}
