8f903fbf9780d1362714a1d8b3895036c7de900a
JasonChoi <ccc11203@gmail.com>
2018-11-16 19:37:16 +0900

    Chapter D_7 PointerParameter Specialization

---
+++ ChapterD07_PointerParameter_Specialization/D_7_PointerParameter_Specialization.cpp
@@ -0,0 +1,71 @@
+/*
+HongJeongMo C++
+
+Chapter D_7 PointerParameter Specialization
+
+*/
+
+#include <iostream>
+using namespace std;
+
+template<class T>
+class A
+{
+private:
+	T m_value;
+
+public:
+	A(const T & input)
+		:m_value(input)
+	{
+	}
+
+	template<typename T>
+	void doSomething()
+	{
+		cout << typeid(T).name() << endl;
+	}
+
+	void print()
+	{
+		cout << m_value << endl;
+	}
+};
+
+
+//PointerParameter Specialization
+template<class T> 
+class A<T*>
+{
+private:
+	T m_value;
+
+public:
+	A(T * input)
+		:m_value(input)
+	{
+	}
+
+	void print()
+	{
+		cout << *m_value << endl;
+	}
+};
+
+
+int main()
+{
+	A<int> a_int(123);
+	a_int.print();
+
+	int temp = 456;
+
+	A<int*> a_int_ptr(&temp);
+	a_int_ptr.print();
+
+	double temp_d = 3.141592;
+	A<double*> a_double_ptr(&temp_d);
+	a_double_ptr.print();
+
+	return 0;
+}
