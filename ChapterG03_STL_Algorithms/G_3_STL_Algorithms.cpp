8c320e89a6bc807f5a76b8ecb5e0953b4db3e6b4
JasonChoi <ccc11203@gmail.com>
2018-11-17 21:55:59 +0900

    Chapter G_3 STL, #include <algorithm>

---
+++ ChapterG03_STL_Algorithms/G_3_STL_Algorithms.cpp
@@ -0,0 +1,49 @@
+/*
+HongJeongMo C++
+
+Chapter G_3 STL, #include <algorithm>
+
+*/
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	vector<int> container;
+	for (int i = 0; i < 10; ++i)
+		container.push_back(i);
+
+	//auto itr = li.begin();
+	//list<int>::const_iterator itr;
+	auto itr = min_element(container.begin(), container.end());
+	cout << *itr << endl;
+
+	itr = max_element(container.begin(), container.end());
+	cout << *itr << endl;
+
+	cout << endl;
+
+	itr = find(container.begin(), container.end(), 3);
+	container.insert(itr, 128);
+
+	for (auto &e : container) cout << e << " ";
+	cout << endl;
+
+	sort(container.begin(), container.end());
+	//li.sort();
+
+	for (auto &e : container) cout << e << " ";
+	cout << endl;
+
+	reverse(container.begin(), container.end());
+	//li.reverse();
+
+	for (auto &e : container) cout << e << " ";
+	cout << endl;
+
+	return 0;
+}
